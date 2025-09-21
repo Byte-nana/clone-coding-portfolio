a4316f171b29b4ebcf7c043f98ebaf3de9c616dd
Byte-nana <nanadevelop44@gmail.com>
2025-09-21 23:34:26 +0100

    JS-Filter

--- src/projects.js
+++ src/projects.js
@@ -7,18 +7,32 @@
 //2.filter and match projects
 
 const categories = document.querySelector('.categories');
-const project = document.querySelectorAll('.project');
+const projects = document.querySelectorAll('.project');
+const category = document.querySelectorAll('.category');
+const projectsContainer = document.querySelector('.projects');
 
 categories.addEventListener('click', (e) => {
   const filter = e.target.dataset.categories;
 
   if (filter == null) return;
 
-  project.forEach((project) => {
+  //reset button
+  const active = document.querySelector('.category--selected');
+  active.classList.remove('category--selected');
+  e.target.classList.add('category--selected');
+
+  //filter projects
+  projects.forEach((project) => {
     if (filter === 'all' || project.dataset.type === filter) {
       project.style.display = 'block';
     } else {
       project.style.display = 'none';
     }
   });
+
+  //animation
+  projectsContainer.classList.add('anim-out');
+  setTimeout(() => {
+    projectsContainer.classList.remove('anim-out');
+  }, 250);
 });
