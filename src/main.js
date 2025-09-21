d6e8829b22831e744d0dbe88ae3be19dd6170712
Byte-nana <nanadevelop44@gmail.com>
2025-09-21 19:47:18 +0100

    JS-Arrow-up

--- src/main.js
+++ src/main.js
@@ -18,3 +18,14 @@ const homeHeight = home.offsetHeight;
 document.addEventListener('scroll', () => {
   home.style.opacity = 1 - window.scrollY / homeHeight;
 });
+
+//Arrow-up fade-out
+const arrowUp = document.querySelector('.arrow-up');
+
+document.addEventListener('scroll', () => {
+  if (window.scrollY > homeHeight / 2) {
+    arrowUp.style.opacity = '1';
+  } else {
+    arrowUp.style.opacity = '0';
+  }
+});
